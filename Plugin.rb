9134961d5eacdc0a172d9368bf1c3b4b3e464708
Ashok Menon <amenon94@gmail.com>
2011-01-16 23:08:51 +0000

    Plugin class partially implemented. Untested

--- Plugin.rb
+++ Plugin.rb
@@ -1,5 +1,14 @@
+# Plugin Class
+# 
+# Used to add functionality to bot
+# through methods called by events
+# managed by the event manager cl-
+# -ass.
 
 class Plugin
+
+	attr_reader :name, :version, :desc
+
 	def initialize name, version, desc
 
 		@name 		 = name
@@ -8,13 +17,22 @@ class Plugin
 
 	end
 
-	def init_proc body
-
+	def init_proc &body
+		@init_proc = body
 	end
 
-	def method name, body
-
+	def loaded
+		@init_proc[self]
 	end
-
 
 end
+
+rbp_test_plugin = Plugin.new(
+ "Test Plugin", "1.0",
+ "A Test Plugin to make sure the plugin
+\ class works")
+
+rbp_test_plugin.init_proc { |plugin|
+	EventManager.add_registrar(plugin,
+	"bot->received_packet")
+}

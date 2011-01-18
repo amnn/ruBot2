52ff0e7f3e8543012be3f2c3925978df108f5e62
Ashok Menon <amenon94@gmail.com>
2011-01-18 22:46:28 +0000

    Made plugin variable . IRCBot.load_plugin finished

--- IRCBot.rb
+++ IRCBot.rb
@@ -1,16 +1,36 @@
-require "./EM/eventmanager.rb"
+require "./Plugin.rb"
+require "./em/EventManager.rb"
 
 class IRCBot
+
+	def initialize
+		@plugins = []
+		EventManager.add_sender(:bot, self)
+		EventManager.add_event(:bot, :received_packet)
+	end
+
 
 	def load_plugin filename
 		raise ArgumentError, "No Such Plugin" if !File.exists? filename
 		
 		if filename =~ /^(?:.+\/)?(.+)\.rb$/i
-			var_name = "rbp_" + $1
-			mod_name = "RBP_" + var_name.split("_").map { |s| s.capitalize }.join("")
+			var_name = "$rbp_" + $1
 		else
 			raise ArgumentError, "Plugin name in incorrect format, \'#{ filename }\'"
 		end
+		
+		require filename
+		if eval( "defined? #{var_name}" ).nil?
+			raise ArgumentError, "Plugin variable not defined in correct format: \'#{var_name}\'"
+		end
+
+		plugin = eval( var_name )
+		plugin.loaded
+
+		@plugins = @plugins | [plugin]
 	end
 
 end
+
+bot = IRCBot.new
+bot.load_plugin("./plugins/test_plugin.rb")
--- plugins/test_plugin.rb
+++ plugins/test_plugin.rb
@@ -1,5 +1,5 @@
-require "../Plugin.rb"
-require "../em/EventManager.rb"
+# require "../Plugin.rb"
+# require "../em/EventManager.rb"
 
 module RBP_TestPlugin						# Put all methods of the plugin in here
 											# Name should be "RBP_" followed by filename,
@@ -14,14 +14,14 @@ module RBP_TestPlugin						# Put all methods of the plugin in here
 	end
 end
 
-rbp_test_plugin = Plugin.new(				# Initialise plugin itself
+$rbp_test_plugin = Plugin.new(				# Initialise plugin itself
 "Test Plugin", "1.0",						# with a name, version and description
 "Plugin to test the functionality\
  of the plugin architecture")				# Variable name should be "rbp_" followed by the
 											# file name, without the file extension
-rbp_test_plugin.init_proc { |_self|			# Use init_proc to register to relevant events
+$rbp_test_plugin.init_proc { |_self|			# Use init_proc to register to relevant events
 	EventManager.add_registrar(_self,
 	"bot->received_packet")
 }
 
-rbp_test_plugin.extend( RBP_TestPlugin )	# Include the previously defined methods to the plugin
+$rbp_test_plugin.extend( RBP_TestPlugin )	# Include the previously defined methods to the plugin

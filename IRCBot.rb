f11ef6b8eaa8d870d23ecf1981240eda3ebb66b1
Ashok Menon <amenon94@gmail.com>
2011-01-18 07:14:13 +0000

    Added rudimentary plugin loading

--- IRCBot.rb
+++ IRCBot.rb
@@ -1,1 +1,16 @@
 require "./EM/eventmanager.rb"
+
+class IRCBot
+
+	def load_plugin filename
+		raise ArgumentError, "No Such Plugin" if !File.exists? filename
+		
+		if filename =~ /^(?:.+\/)?(.+)\.rb$/i
+			var_name = "rbp_" + $1
+			mod_name = "RBP_" + var_name.split("_").map { |s| s.capitalize }.join("")
+		else
+			raise ArgumentError, "Plugin name in incorrect format, \'#{ filename }\'"
+		end
+	end
+
+end
